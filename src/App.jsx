7e2e0285fb59ea01667290146bb3e25c63709b83
ericpkatz <ericpkatz@gmail.com>
2024-03-04 14:53:17 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,89 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import bookLogo from './assets/books.png'
+import Books from './components/Books';
+import Login from './components/Login';
 
 function App() {
-  const [token, setToken] = useState(null)
+  const [books, setBooks] = useState([]);
+  const [auth, setAuth] = useState({});
+  const [reservations, setReservations] = useState([]);
+
+  useEffect(()=> {
+    const attemptLoginWithToken = async()=> {
+      const response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/me', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      const json = await response.json();
+      if(response.ok){
+        setAuth(json);
+      }
+    };
+    const token = window.localStorage.getItem('token');
+    if(token){
+      attemptLoginWithToken();
+    }
+  }, []);
+
+
+
+
+  const login = async(credentials)=> {
+    let response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify(credentials),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    let json = await response.json();
+    if(response.ok){
+      const token = json.token;
+      window.localStorage.setItem('token', token);
+      response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/me', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      json = await response.json();
+      if(response.ok){
+        setAuth(json);
+      }
+    }
+    else {
+      console.log(json);
+    }
+  }
+
+  const logout = ()=> {
+    window.localStorage.removeItem('token');
+    setAuth({});
+  };
+
+  useEffect(()=> {
+    const fetchBooks = async()=> {
+      const response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books');
+      const json = await response.json();
+      setBooks(json.books);
+    };
+    fetchBooks();
+  }, []);
+
 
   return (
     <>
       <h1><img id='logo-image' src={bookLogo}/>Library App</h1>
+      {
+        auth.id ? (
+          <button onClick={ logout }>Welcome {auth.email } (Click to logout)</button>
+          ):
+          (
+            <Login login={ login }/> 
+          )
+      }
+      <Books books={ books } />
 
-      <p>Complete the React components needed to allow users to browse a library catalog, check out books, review their account, and return books that they've finished reading.</p>
-
-      <p>You may need to use the `token` in this top-level component in other components that need to know if a user has logged in or not.</p>
-
-      <p>Don't forget to set up React Router to navigate between the different views of your single page application!</p>
     </>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,1 +1,32 @@
-/* TODO - add your code to create a functional React component that renders a login form */
+import { useState } from 'react';
+const Login = ({ login })=> {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submit = async(ev)=> {
+    ev.preventDefault();
+    const credentials = {
+      email,
+      password
+    };
+    await login(credentials);
+  }
+
+  return (
+    <form onSubmit={ submit }>
+      <input
+        placeholder='email'
+        value={ email }
+        onChange={ ev => setEmail(ev.target.value )}
+      />
+      <input
+        placeholder='password'
+        value={ password }
+        onChange={ ev => setPassword(ev.target.value )}
+      />
+      <button>Login</button>
+    </form>
+  );
+};
+
+export default Login;
